4ccabc2625c6ab91faae15738fc52f152db8e833
sethyadav <sethyadav169@gmail.com>
2026-04-14 17:04:08 +0530

    feat: initialize frontend project with React, Vite, Tailwind CSS, and React Router configuration

--- Snitch/Frontend/src/app/App.jsx
+++ Snitch/Frontend/src/app/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from './assets/vite.svg'
-// import heroImg from './assets/hero.png'
 import './App.css'
+import { RouterProvider } from 'react-router'
+import { routes } from './app.routes'
 
 function App() {
+
   return (
    <>
-   <h1>hello</h1>
+     <RouterProvider router={routes} />
    </>
   )
 }
